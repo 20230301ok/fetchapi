512bc73f2d7009f6e9025d0a3b72b6102bafeb1e
Nat <natgochez@gmail.com>
2025-06-02 15:39:40 -0600

    funcionalidad completa

--- fetchApi2b - copia/js/script.js
+++ fetchApi2b - copia/js/script.js
@@ -1,41 +1,96 @@
-//URL DE LA API
-const API_URL = "https://retoolapi.dev/msEo4e/expo"
-
-//Funcion para llamar a la api y traer el json
-async function  ObtenerPersonas(){
-    //Obtenemos la respuesta del servidor
-    const res = await fetch(API_URL);
-    //Convertir la respuesta del servidor a formato JSON
-    const data = await res.json(); //esto es un json
-
-    CrearTabla(data); //Enviamos el JSON a la funcion "CrearTbbbla"
+//URL DE LA API - ENDPOINT
+ 
+const API_URL = "https://retoolapi.dev/msEo4e/expo";
+ 
+//Funcion para llamr a la API y traer el JSON
+async function ObtenerPersonas() {
+    //obtenemos la respuesta del servidor
+    const res = await fetch(API_URL)
+    //cONVERTIR LA RESPUESTA DEL SERVIDOR A FORMATO JSON
+ 
+    const data = await res.json();
+ 
+    CrearTabla(data);//Enviamos el JSON  a la funicon creartabla
 }
-
-//Funcion que creara las filas de la tabla en base a los registros que vienen de la api
-
-function CrearTabla(datos){ //"Datos" representa al JSON que viene de la API
-    //Se llama al tbody dentro de la tabla "tabña"
+ 
+//Funcion que creara las filas de la tabla en base a los registros que vienen de la API
+function CrearTabla(datos){//Datos representa al JSON que viene de la API
+ 
+    //Se llama "tbody" dentro de la tabla con id "tabla"
     const tabla = document.querySelector("#tabla tbody");
-
-    //Para inyectar codigo HTML usamos "innerHMTL"
-    tabla.innerHTML = ""; //Vaciamos el contenido de la tabla
-
-    datos.forEach(persona =>{
+ 
+    //Para inyectar codigo HTML usamos "innerHTML"
+    tabla.innerHTML = "";//Vaciamos el contenido de la tabla
+ 
+    datos.forEach( persona => {
         tabla.innerHTML += `
-            <tr>
-                 <td>${persona.id}</td>
-                 <td>${persona.nombre}</td>
-                 <td>${persona.apellido}</td>
-                 <td>${persona.edad}</td>
-                 <td>${persona.correo}</td>
-                 <td>
-                 <button>Editar</button>
-                 <button>Eliminar</button>
+         <tr>
+            <td>${persona.id}</td>
+             <td>${persona.nombre}</td>
+             <td>${persona.apellido}</td>
+             <td>${persona.edad}</td>
+            <td>${persona.correo}</td>
+            <td>
+                    <button>Editar</button>
+                    <button>Eliminar</button>
                  </td>
-             </tr>
+         </tr>
+       
         `
     });
-
+ 
 }
-//Se llama obtene personas
 ObtenerPersonas();
+ 
+//proceso para agregar un nuevo registro
+const modal = document.getElementById("modalAgregar");
+const btnAgregar = document.getElementById("btnAbrirModal");
+const btnCerrar = document.getElementById("btnCerrarModal");
+ 
+btnAgregar.addEventListener("click", ()=>{
+    modal.showModal();
+});
+ 
+btnCerrar.addEventListener("click", ()=>{
+    modal.close();
+});
+ 
+document.getElementById("frmAgregarIntegrante").addEventListener("submit", async e =>{
+    e.preventDefault();
+ 
+ 
+    const nombre = document.getElementById("nombre").value.trim();
+    const apellido = document.getElementById("apellido").value.trim();
+    const edad = document.getElementById("edad").value.trim();
+    const correo = document.getElementById("email").value.trim();
+   
+ 
+    if(!nombre || !apellido || !correo || !edad){
+        alert("Complete todos los campos");
+        return;
+    }
+ 
+    const respuesta = await fetch(API_URL, {
+        method: "POST",
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify({nombre,apellido,edad,correo})
+    });
+ 
+    if(respuesta.ok){
+   
+        alert("El registro fue agregado correctamente")
+       
+        document.getElementById("frmAgregarIntegrante").reset();
+   
+        modal.close();
+ 
+        ObtenerPersonas();
+ 
+   
+    }
+    else{
+            alert("Hubo un error al agregar");
+        }
+ 
+ 
+});//fin del formulario
