a0f397358ea039e2f4032aadd7c4daa577f4b504
nat <natgochez@gmail.com>
2025-06-03 09:52:32 -0600

    eliminar registro

--- fetchApi2b - copia/js/script.js
+++ fetchApi2b - copia/js/script.js
@@ -31,8 +31,8 @@ function CrearTabla(datos){//Datos representa al JSON que viene de la API
              <td>${persona.edad}</td>
             <td>${persona.correo}</td>
             <td>
-                    <button>Editar</button>
-                    <button>Eliminar</button>
+                    <button onClick="ActualizarRegistro(${persona.id})">Editar</button>
+                    <button onClick="EliminarRegistro(${persona.id})">Eliminar</button>
                  </td>
          </tr>
        
@@ -94,3 +94,16 @@ document.getElementById("frmAgregarIntegrante").addEventListener("submit", async
  
  
 });//fin del formulario
+
+//Para eliminar registros
+
+async function EliminarRegistro(id){ //Se pide el Id para borrar u_u
+    if(confirm("¿Esta seguro que desea borrar?")){
+        await fetch(`${API_URL}/${id}`, {method: `DELETE` }); //Se consigue la Id y se elimina
+        ObtenerPersonas(); //Para obtner personas
+    }
+}
+
+async function ActualizarRegistro(id, nombre, apellido, email, edad){
+    
+}
